6f24faa43d9e7052f00128678dec255d86d05c1b
Pame2394 <pame2394@gmail.com>
2022-07-17 21:53:10 -0600

    done

---
+++ random-color.js
@@ -0,0 +1,11 @@
+function getNewColor(){
+    let symbols, color;
+    symbols = "123456789ABCDEF";
+
+    color = "#";
+    for (let i =0; i<6;i++){
+        color = color + symbols[Math.floor(Math.random() * 16)]
+    }
+    document.body.style.background = color;
+    document.getElementById("hex").innerHTML = color;
+}
